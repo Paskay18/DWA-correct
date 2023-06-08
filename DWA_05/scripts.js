978f6c22344bf846bdd718e47dc6a11d16c71281
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-06-08 09:59:24 +0200

    DWA5

--- DWA_05/scripts.js
+++ DWA_05/scripts.js
@@ -4,14 +4,14 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+   event.preventDefault();
+   const entries = new FormData(event.target);
+   const { dividend, divider } = Object.fromEntries(entries);
+   result.innerText = dividend / divider;
 });
 
-//decimal number
-form.addEventListener("submit", (event)=>{
+//decimal number to appear as a whole number
+form.addEventListener("submit", (event)=> {
     event.preventDefault();
     const decimalNumber = new FormData(event.target);
     const {dividend, divider} = Object.fromEntries(decimalNumber);
@@ -20,23 +20,25 @@ form.addEventListener("submit", (event)=>{
 
 //if either or both are empty no division is done should be displayed
 
-form.addEventListener("submit",(event)=>{
+form.addEventListener("submit",(event)=> {
     event.preventDefault();
 
     const inputs = new FormData(event.target);
     const { dividend, divider} = Object.fromEntries(inputs)
  
     try{
-        if (dividend === ''){
+      if (dividend === '') {
            throw 'Division not performed. Both values are required in inputs.Try again'
         }
-        if(divider ===''){
+        
+      if (divider ==='') {
            throw 'Division not performed. Both values are required in inputs.Try again'
         }
-        if(dividend && divider === ''){
+     
+      if(dividend && divider === '') {
             throw 'Division not performed. Both values are required in inputs.Try again'
         }
-    }catch (error){
+    } catch (error) {
         result.innerText = error
         console.log(error)
     };
@@ -46,14 +48,14 @@ form.addEventListener("submit",(event)=>{
 
 //Invalid division that logs error in console
 
-form.addEventListener("submit",(event)=>{
+form.addEventListener("submit",(event)=> {
     event.preventDefault();
 
     const inputs = new FormData(event.target);
     const { dividend, divider} = Object.fromEntries(inputs)
  
     try{
-        if (dividend > 0 && divider < 0){
+        if (dividend > 0 && divider < 0) {
            throw 'Division not performed. Invalid number provided. Try again'
         }
         
@@ -67,7 +69,7 @@ form.addEventListener("submit",(event)=>{
 
 //providing anything that is not a number 
 
-form.addEventListener('submit',(event)=>{
+form.addEventListener('submit',(event)=> {
     event.preventDefault();
     
     
@@ -76,7 +78,7 @@ form.addEventListener('submit',(event)=>{
  
    
   
-        if (isNaN(dividend)|| isNaN(divider)){
+        if (isNaN(dividend)|| isNaN(divider)) {
             document.body.innerHTML = "Something critical went wrong. Please reload the page"
         }
      console.log('Inputs that were put are not numbers')
