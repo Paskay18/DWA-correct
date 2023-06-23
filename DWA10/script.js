106492c8774516175be0236b347e2388c5d5ad3e
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-06-23 15:52:19 +0200

    DWA 10
    DWA10 solutions

---
+++ DWA10/script.js
@@ -0,0 +1,54 @@
+const STEP_AMOUNT =5
+const MIN_NUMBER = -5
+
+
+const number=document.querySelector('[data-key="number"]')
+const substract=document.querySelector('[data-key="subtract"]')
+const add=document.querySelector('[data-key="add"]')
+const reset =document.querySelector('[data-key="Reset"]')
+
+
+const substractHandler = () => {
+    const newValue= parseInt(number.value)-1
+    number.value=newValue
+
+    if(add.disabled===true){
+        add.disabled=false
+    }
+     
+        if(newValue<=MIN_NUMBER){
+            substract.disabled=true
+        }
+}
+
+
+const addHandler = () => { 
+    const newValue= parseInt(number.value)+1
+    number.value=newValue
+
+
+     if(substract.disabled===true){
+        substract.disabled=false 
+    }
+    
+}
+substract.addEventListener('click', substractHandler)
+add.addEventListener('click', addHandler)
+
+
+//add reset button
+const resetHandler = () => {
+    
+
+if (number.value){
+        number.value = 0
+        window.alert('Counter has been reset') 
+   }
+    
+        
+}
+
+
+reset.addEventListener('click', resetHandler)
+
+
