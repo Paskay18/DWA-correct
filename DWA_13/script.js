984caf0ca81698c8a33e045b98894ab1490334df
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-06-30 15:55:34 +0200

    DWA 13 solutions

--- DWA_13/script.js
+++ DWA_13/script.js
@@ -3,27 +3,24 @@ const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikki
 
 
 //calling each name and province
-
 names.forEach((names, i) => {
-    console.log(`${names}`.concat(`(${provinces[i]})`))
+console.log(`${names}`.concat(`(${provinces[i]})`))
     })
     
-    //names
-    names.forEach((names) => {
-        console.log(names)
+//names
+names.forEach((names) => {
+console.log(names)
     })
     
     
-    //filter
-    
-    const filterCapeProvinces = provinces.filter((cape)=> {
+//filter
+const filterCapeProvinces = provinces.filter((cape)=> {
         return !cape.includes('Cape')
     })
-    console.log(filterCapeProvinces)
+console.log(filterCapeProvinces)
     
 
 //map of names to uppercase
-
 const toUpperCase = provinces.map(upperCase => {return upperCase.toUpperCase()} )
 console.log(toUpperCase)
 
@@ -32,22 +29,39 @@ console.log(toUpperCase)
 const toNumbers = names.map( (numbers) =>{
     return numbers.length
     })
+console.log(toNumbers)
     
-    console.log(toNumbers)
-    
-    //sort
-     const sortNames = provinces.sort( (a,b)  => {
+//sort
+const sortNames = provinces.sort( (a,b)  => {
         if (a < b) {return -1;}
         if (b > a) {return 1;}
         return 0;
         
       } );
-     console.log(sortNames)
+console.log(sortNames)
 
- //map and some
+//map and some
+const toBoolean = names.map( (name) => name.split('').some(char => char.toLowerCase() == 's'))
+console.log(toBoolean)
 
- const toBoolean = names.map( (name) => name.split('')
-      .some(char => char.toLowerCase() == 's'))
+//using reduce
 
-    console.log(toBoolean)
+const object = names.reduce((name, obj, index) => {
+    obj[name] = provinces[index];
+    return obj;
+}, {});
+console.log(object)
+
+
+//Additional questions
+
+//each name
+
+products.forEach(product => console.log( product.product))
+
+
+//filter that has longer than 5 characters
+
+const filter5 = products.filter( char5 => char5.product.length> 5 );
+console.log(filter5)   
 
